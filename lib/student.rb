99d7a54a52171221b16f3149b70f36f518e7aa44
Nate Miller <natekmiller96@gmail.com>
2018-07-28 02:44:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,6 +64,14 @@ class Student
     end.first
   end
 
+  def self.new_from_db(row)
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    # student.id
+    student
+  end
 
 
 end
