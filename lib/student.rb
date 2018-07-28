74347993d5eaa3d7529702e7996ac777e0971c03
Nate Miller <natekmiller96@gmail.com>
2018-07-28 02:26:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,13 +34,13 @@ class Student
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
     student
   end
 
-  
-  
+
+
 end
