7afe3b2e7c487a883c76b0d52bcc7ab53c3c651f
Nate Miller <natekmiller96@gmail.com>
2018-07-28 02:29:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,11 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    if @id != nil
+      @id
+    else
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
   end
 
   def self.create(name:, grade:)
